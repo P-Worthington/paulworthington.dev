800ae6ead27875dec15f92af3556600c851acfed
Paul Worthington <pworthington17@gmail.com>
2024-07-17 10:34:23 +0000

    git api complete

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -22,7 +22,7 @@ function repoInformationHTML(repos) {
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-                    <a href="${repo.html_url}" target="_blank">${repo.name} • </a>
+                   <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`;
     });
 
