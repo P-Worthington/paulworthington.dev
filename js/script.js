c92d11e02137ecbc0030783e2f13a5f1dcb15fdb
Paul Worthington <pworthington17@gmail.com>
2024-08-25 15:57:27 +0000

    docs complete

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function docsWindowOpen() {
+    document.getElementById("docs-window").style.display = "block";
+}
+
+function docsWindowClose() {
+    document.getElementById("docs-window").style.display = "none";
+}
+
+function expWindowOpen() {
+    document.getElementById("experience-window").style.display = "block";
+}
+
+function expWindowClose() {
+    document.getElementById("experience-window").style.display = "none";
+}
+
+function proWindowOpen() {
+    document.getElementById("projects-window").style.display = "block";
+}
+
+function proWindowClose() {
+    document.getElementById("projects-window").style.display = "none";
+}
+
+function aboutWindowOpen() {
+    document.getElementById("about-window").style.display = "block";
+}
+
+function aboutWindowClose() {
+    document.getElementById("about-window").style.display = "none";
+}
+
+function langWindowOpen() {
+    document.getElementById("lang-window").style.display = "block";
+}
+
+function langWindowClose() {
+    document.getElementById("lang-window").style.display = "none";
+}
+
+function contactWindowOpen() {
+    document.getElementById("contact-window").style.display = "block";
+}
+
+function contactWindowClose() {
+    document.getElementById("contact-window").style.display = "none";
+}
+
+function resumeWindowOpen() {
+    document.getElementById("resume-window").style.display = "block";
+}
+
+function resumeWindowClose() {
+    document.getElementById("resume-window").style.display = "none";
+}
